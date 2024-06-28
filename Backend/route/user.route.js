c5d95c1a14381f550eeec599e38607112d8feef1
Abhigyan Shrivastava <97295401+Abhigyan2212@users.noreply.github.com>
2024-06-28 11:55:14 +0530

    Delete Backend directory

--- Backend/route/user.route.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { signup, login } from "../controller/user.controller.js";
-const router = express.Router();
-
-router.post("/signup", signup);
-router.post("/login", login);
-
-export default router;
