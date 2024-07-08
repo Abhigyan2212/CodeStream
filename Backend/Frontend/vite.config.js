d1f505fb6e258c8d653a109c2c96bc88b228792e
Abhigyan Shrivastava <97295401+Abhigyan2212@users.noreply.github.com>
2024-07-08 10:56:30 +0530

    Update vite.config.js

--- Backend/Frontend/vite.config.js
+++ Backend/Frontend/vite.config.js
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    outDir: 'dist'
+  },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'mongodb+srv://abhigyanshr2122:abhigyan1234@cluster0.qaaubik.mongodb.net/',
+        changeOrigin: true
+      }
+    }
+  }
+});
